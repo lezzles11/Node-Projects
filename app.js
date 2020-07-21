044023e7cc2520343d531669df462acae1377d15
lesleyc@bu.edu <lesleyc@bu.edu>
2020-07-21 12:03:13 +0800

    handlebars works

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
-var cookieParser = require("cookie-parser");
 var nodemailer = require("nodemailer");
-var exphbs = require("express-handlebars");
-var logger = require("morgan");
 
 var app = express();
 
@@ -12,9 +9,8 @@ var app = express();
 // Can define the layouts
 // dirname always points to root directory
 
-app.engine("handlebars", exphbs());
-app.set("view engine", "handlebars");
-// Folder name is views
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 // Set it up as the view engine
 
@@ -22,14 +18,51 @@ app.set("view engine", "handlebars");
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function (req, res) {
   console.log("This will show up on the console");
   // server -> sending back to client
-  res.render("home");
+  res.render("index");
 });
 
+app.get("/contact", function (req, res) {
+  console.log("Rendering contact form ");
+  // server -> sending back to client
+  res.render("contact");
+});
+
+app.get("/about", function (req, res) {
+  console.log("Rendering contact form ");
+  // server -> sending back to client
+  res.render("about");
+});
+
+var mailOptions = {
+  from: "Lesley <crappyfirstdrafts123@gmail.com",
+  to: "lesley.c@xccelerate.co",
+  subject: "yo",
+  text: "is this working",
+  html: "<p>yoyoyoy</p>",
+};
+app.post("/contact/send", function (req, res) {
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "crappyfirstdrafts123@gmail.com",
+      pass: "ASDF1234!!",
+    },
+  });
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+      res.redirect("/");
+    } else {
+      console.log("Message sent" + info.response);
+      res.resdirect("/");
+    }
+  });
+  console.log("Testing post contact/send");
+});
 app.listen(3000);
 console.log("Server running on port 3000");
